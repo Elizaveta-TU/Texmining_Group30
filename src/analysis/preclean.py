9beb63c3a4d12bd44243ab4bd3cdbf1ba9bdc68e
Elizaveta-TU <63558103+Elizaveta-TU@users.noreply.github.com>
2020-06-04 22:43:57 +0300

    deleted output_figures folder

--- src/analysis/preclean.py
+++ src/analysis/preclean.py
@@ -5,7 +5,7 @@ import os
 
 os.makedirs('../../gen/analysis/temp/', exist_ok=True)
 os.makedirs('../../gen/analysis/output/', exist_ok=True)
-os.makedirs('../../output_figures/', exist_ok=True)
+
 
 df = pd.read_csv('../../gen/data-preparation/output/dataset.csv')
 
